1d9e25255cbd71b561370a94281db360319f6540
Matt He <matthlh123@gmail.com>
2021-09-12 11:07:15 -0400

    Done a lot of things. Been working on codesandbox.io To recap... 1. Fixed POST and GET request

--- src/TinderCards.js
+++ src/TinderCards.js
@@ -46,7 +46,12 @@ const useStyles = makeStyles({
 
 const TinderCards = () => {
   const classes = useStyles();
-  const [movies, setMovies] = useState([])
+  const [movies, setMovie] = useState([
+    {
+      "summary": "Welcome to Tinder for Movies, swipe right or left to get started!"
+    }
+
+  ]);
   let lastDirection = ""
 
   const onSwipe = (direction) => {
@@ -58,38 +63,61 @@ const TinderCards = () => {
     console.log(myIdentifier + " left the screen");
       fetch('/api/create', {
         method: 'POST',
-        body: JSON.stringify({
-          // content: {lastDirection === 'left' ? 'y' : 'n'
-          }),
+        body: lastDirection
+      }).then(response => {
+        return response.json()
+      }).then(message => {
+        console.log(message)
+        getLatestSummary()
       })
   }
 
+  const getLatestSummary = () => {
+    fetch('/api').then(response => {
+      if(response.ok) {
+        // console.log(response.json())
+        return response.json()
+      }
+    }).then(data => {
+      console.log(data)
+      setMovie(data.data)
+    })
+  }
+
   useEffect(() => {
-    fetch('/api').then(response => response.json()).then(data => setMovies(Array.from(data)))
+    fetch('/api').then(response => {
+      if(response.ok) {
+        // console.log(response.json())
+        return response.json()
+      }
+    }).then(data => {
+      console.log(data)
+      setMovie(data.data)
+    })
   },[]);
 
-  return (
-    <div className={classes.container}>
-      {movies.map((movie) => (
+    console.log(movies)
+    return (
+      <div className={classes.container}>
+        {movies.map((movie) => (
         <TinderCard
-          className={classes.swipe}
-          onSwipe={onSwipe}
-          onCardLeftScreen={() => onCardLeftScreen(movie.title)}
-          preventSwipe={["up", "down"]}
-          key={movie}
-          backgroundColor="white"
-        >
-          <div className={classes.card}>
-            {/*<div*/}
-            {/*  style={{ backgroundImage: `url(${movie.url})` }}*/}
-            {/*  className={classes.image} >*/}
-            {/*</div>*/}
-            <Typography className={classes.text}>{movie.summary}</Typography>
-          </div>
-        </TinderCard>
-      ))}
-    </div>
-  );
+            className={classes.swipe}
+            preventSwipe={["up", "down"]}
+            onSwipe={onSwipe}
+            onCardLeftScreen={() => onCardLeftScreen("Card")}
+            backgroundColor="white"
+          >
+            <div className={classes.card}>
+              <div
+                style={{ backgroundImage: `url(${'https://images.unsplash.com/photo-1569428034239-f9565e32e224?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1516&q=80'})` }}
+                className={classes.image} >
+              </div>
+              <Typography className={classes.text}>{movie.summary}</Typography>
+            </div>
+          </TinderCard>
+            ))}
+      </div>
+    );
 };
 
 export default TinderCards;
